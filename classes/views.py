7b48a9c60c440e68688b7e861fda7e11e57ed963
Andrew <balshowandrew@gmail.com>
2022-10-01 20:28:57 +0300

    eni changes in result.html

--- classes/views.py
+++ classes/views.py
@@ -49,7 +49,7 @@ def get_query(request):
 
 
 def get_result(request):
-    query = Queries.objects.last()
+    query = Queries.objects.order_by('pk').last()
     context = {
         'title': 'Результат запроса',
         'query': query
